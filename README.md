0043db8bf4485c5bf7e9077736293b05e3535cf5
churromancer <churromancer@gmail.com>
2018-03-15 10:13:47 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# mychefclient
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: mychefclient
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
