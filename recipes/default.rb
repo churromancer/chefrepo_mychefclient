13fbdc9f2742b1b32e0ffc1e086e78c1eca536a1
churromancer <churromancer@gmail.com>
2018-03-22 12:48:22 -0600

    Initial upload of mychefclient cookbook from chef-repo (chef server)

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['chef_client']['interval'] = '300'
+node.default['chef_client']['splay'] = '60'
+
+include_recipe 'chef-client::default'
